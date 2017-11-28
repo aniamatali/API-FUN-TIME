c134a197805501e556aaeea2fa67f01829d2d9a4
alvin niamatali and hansen hansen <alvin@>
2017-11-28 08:26:49 -0800

    added a list of cities humidities with a press of a button

--- js/script-interface.js
+++ js/script-interface.js
@@ -1,26 +1,33 @@
 var apiKey = require('./../.env').apiKey;
 
+let capitals = ["seattle", "portland", "new york", "wichita", "austin", "baltimore", "augusta", "atlanta", "raleigh", "chicago", "sacramento", "tampa", "albany"];
+let capitals2 = [];
+
+capitals.forEach(function(string){
+  string = string.toUpperCase();
+  capitals2.push(string);
+});
 
 $(document).ready(function() {
 
   let score = 0;
 
   setTimeout(function(){
-  $(".question1").hide();
-  $(".question2").show();
-  $(".buttons").show();
+    $(".question1").hide();
+    $(".question2").show();
+    $(".buttons").show();
   }, 5000);
 
   setTimeout(function(){
-  $(".question2").hide();
-  $(".question3").show();
-  $(".buttons").show();
+    $(".question2").hide();
+    $(".question3").show();
+    $(".buttons").show();
   }, 10000);
 
   setTimeout(function(){
-  $(".question3").hide();
-  $(".score").show();
-  $("#yourscore").text(score);
+    $(".question3").hide();
+    $(".score").show();
+    $("#yourscore").text(score);
   }, 15000);
 
   $(".correct").click(function(){
@@ -47,7 +54,7 @@ $(document).ready(function() {
         let response = JSON.parse(this.responseText);
         getElements(response);
       }
-    }
+    };
 
     request.open("GET", url, true);
     request.send();
@@ -56,6 +63,31 @@ $(document).ready(function() {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
       $('.showSpeed').text(`The speed in miles is ${response.wind.speed} mph.`);
-    }
+      $('.showClouds').text(`The cloud coverage is ${response.clouds.all} %`);
+    };
+  });
+});
+
+$(document).ready(function(){
+
+
+  $('#majorCities').click(function() {
+    capitals2.forEach(function(city) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+      request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          let response = JSON.parse(this.responseText);
+          getElements(response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+
+      const getElements = function(response) {
+        $('.city1').append("<li>"+`Humidity in ${city} is ${response.main.humidity}%`+"</li>");
+      }
+    });
   });
 });
